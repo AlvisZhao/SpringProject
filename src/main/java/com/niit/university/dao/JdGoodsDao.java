929ef58cc0d11578523e06377834e12491138ac4
AlvisZhao <450391892@qq.com>
2020-05-14 01:49:14 +0800

    后台

---
+++ src/main/java/com/niit/university/dao/JdGoodsDao.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.niit.university.dao;
+
+import java.util.List;
+
+import com.niit.university.pojo.JdGoodsInfo;
+
+public interface JdGoodsDao {
+	/**
+	 * @category 保存京东商品
+	 * @param jdGoodsInfo
+	 */
+	void save(JdGoodsInfo jdGoodsInfo);
+	/**
+	 * @category 全部查询
+	 */
+	List<JdGoodsInfo> qeury();
+	
+	/**
+	 * @category 清空表
+	 */
+	void delete();
+}
